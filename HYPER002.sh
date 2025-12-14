579ff93afa5f353f2eb0eec54c28bda1ac9f115b
Etienne Rodrigue <3812081+EtienneRod@users.noreply.github.com>
2025-12-14 14:48:06 -0500

    Update HYPER002.sh

--- HYPER002.sh
+++ HYPER002.sh
@@ -10,6 +10,7 @@ export PATH="$PATH:test";
   # $pushoveremail
   # $myemail
   # $hcpingurl
+  # $beszelapi
 export $(grep -v '^#' variables.env | xargs);
 
 echo -e "$password\n$password" | passwd;
@@ -64,6 +65,15 @@ chown Etienne:Etienne /mnt/Share;
 echo $'\n# Share\n//nas.home.famillerg.com/Share /mnt/Share cifs rw,username=Etienne,password=$password,uid=1000,gid=1000 0 0' >> /etc/fstab;
 systemctl daemon-reload && mount -a;
 
+ln -s /usr/bin/rclone /sbin/mount.rclone;
+
+mkdir -p /home/Etienne/HK/RClone;
+cp -rp /mnt/Share/Configurations/HYPER/HYPER001/Etienne/HK/RClone/rclone.conf /home/Etienne/HK/RClone/rclone.conf;
+chown -R Etienne:Etienne /home/Etienne/HK;
+mkdir -p /mnt/Proxmox-Backup && chown Etienne:Etienne /mnt/Proxmox-Backup;
+echo $'\n# S3 Proxmox Etienne Backup\nProxmox:proxmox /mnt/Proxmox-Backup rclone rw,noauto,nofail,_netdev,x-systemd.automount,args2env,vfs_cache_mode=writes,config=/home/Etienne/HK/RClone/rclone.conf,cache_dir=/var/cache/rclone,allow_non_empty 0 0' >> /etc/fstab;
+systemctl daemon-reload && mount -a;
+
 cp /mnt/Share/Configurations/HYPER/HVM/Includes/00_info.sh /etc/profile.d/00_info.sh;
 chown -R root:root /etc/profile.d/00_info.sh;
 echo "" > /etc/motd;
@@ -73,6 +83,17 @@ scp Etienne@172.27.27.32:/mnt/Share/Configurations/SSH/authorized_keys /home/Eti
 scp Etienne@172.27.27.32:/mnt/Share/Configurations/SSH/id_rsa.pub /home/Etienne/.ssh/id_rsa.pub;
 scp Etienne@172.27.27.32:/mnt/Share/Configurations/SSH/id_rsa /home/Etienne/.ssh/id_rsa;
 chown -R Etienne:Etienne /home/Etienne/.ssh && chmod -R 600 /home/Etienne/.ssh/*;
+
+sed -i 's/relayhost =/relayhost = smtp.home.famillerg.com/g' /etc/postfix/main.cf && systemctl restart postfix;
+
+curl -sL https://get.beszel.dev -o /tmp/install-agent.sh && chmod +x /tmp/install-agent.sh && /tmp/install-agent.sh -k \
+f"{beszelapi}" -t a953-da4793d33-21df3-4cdb6549a --auto-update
+mkdir -p /etc/systemd/system/beszel-agent.service.d;
+cat << EOF > /etc/systemd/system/beszel-agent.service.d/override.conf
+[Service]
+Environment="EXTRA_FILESYSTEMS=/VM"
+EOF
+systemctl daemon-reload && systemctl restart beszel-agent.service;
 
 (crontab -u root -l ; echo "MAILTO=$pushoveremail") | crontab -u root -;
 (crontab -u root -l ; echo "MAILFROM=$myemail") | crontab -u root -;
