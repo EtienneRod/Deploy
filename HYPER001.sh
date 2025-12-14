feea1257db2665f3f87cadbdeab9366043991aa7
Etienne Rodrigue <3812081+EtienneRod@users.noreply.github.com>
2025-12-14 14:46:13 -0500

    Update HYPER001.sh

--- HYPER001.sh
+++ HYPER001.sh
@@ -10,12 +10,11 @@ export PATH="$PATH:test";
   # $pushoveremail
   # $myemail
   # $hcpingurl
+  # $beszelapi
 export $(grep -v '^#' variables.env | xargs);
 
-
-echo -e "$password\n$password" | passwd;
-
-apt update && apt -y upgrade && apt install -y sudo vim rclone duperemove;
+apt update && apt -y upgrade && apt install -y sudo vim ffmpeg duperemove;
+curl -O https://github.com/rclone/rclone/releases/download/v1.72.0/rclone-v1.72.0-linux-amd64.deb && sudo apt install ./rclone-v1.72.0-linux-amd64.deb.deb;
 
 cat <<EOF > /etc/profile.d/00_info.sh
 echo -e "    🖥️    OS: \$(cat /etc/os-release | grep ^NAME= | cut -d '=' -f 2 | cut -d '"' -f 2) - Version: \$(cat /etc/os-release | grep ^VERSION= | cut -d '=' -f 2 | cut -d '"' -f 2)";
@@ -60,9 +59,16 @@ EOF
 
 systemctl restart ssh.service;
 
-mkdir -p /mnt/Share;
-chown Etienne:Etienne /mnt/Share;
+ln -s /usr/bin/rclone /sbin/mount.rclone;
+
+mkdir -p /mnt/Share/History/BUREAU-ETIENNE && chown Etienne:Etienne /mnt/Share/History/BUREAU-ETIENNE;
+mkdir -p /mnt/Proxmox-Backup && chown Etienne:Etienne /mnt/Proxmox-Backup;
+mkdir -p /mnt/Share && chown Etienne:Etienne /mnt/Share;
+
 echo $'\n# Share\nUUID=47fbd8fb-5a8e-40c2-89b5-2ad5972acaaf /mnt/Share btrfs space_cache=v2,noatime,nodiratime,autodefrag 0 0' >> /etc/fstab;
+echo $'\n# S3 Proxmox Etienne Backup\nProxmox:proxmox /mnt/Proxmox-Backup rclone rw,noauto,nofail,_netdev,x-systemd.automount,args2env,vfs_cache_mode=writes,config=/home/Etienne/HK/RClone/rclone.conf,cache_dir=/var/cache/rclone,allow-other 0 0' >> /etc/fstab;
+echo $'\n# S3 Desktop Etienne Backup\nDesktop-Etienne:desktop-etienne /mnt/Share/History/BUREAU-ETIENNE rclone uid=1000,gid=1000,rw,noauto,nofail,_netdev,x-systemd.automount,args2env,vfs_cache_mode=writes,config=/home/Etienne/HK/RClone/rclone.conf,cache_dir=/var/cache/rclone,allow_non_empty 0 0' >> /etc/fstab;
+
 systemctl daemon-reload && mount -a;
 
 mkdir -p /home/Etienne/.ssh && chmod 700 /home/Etienne/.ssh;
@@ -74,14 +80,24 @@ chown -R Etienne:Etienne /home/Etienne/.ssh && chmod -R 600 /home/Etienne/.ssh/*
 cp -rp /mnt/Share/Configurations/HYPER/HYPER001/smb.conf /etc/samba/smb.conf;
 chown root:root /etc/samba/smb.conf;
 systemctl enable --now smb nmb && systemctl restart --now smb nmb;
-echo -e "$password\n$password" | smbpasswd -s -a Etienne;
+echo -e "{password}\n{password}" | smbpasswd -s -a Etienne;
 
-(crontab -u root -l ; echo "MAILTO=$pushoveremail") | crontab -u root -;
-(crontab -u root -l ; echo "MAILFROM=$myemail") | crontab -u root -;
+sed -i 's/relayhost =/relayhost = smtp.home.famillerg.com/g' /etc/postfix/main.cf && systemctl restart postfix;
+
+curl -sL https://get.beszel.dev -o /tmp/install-agent.sh && chmod +x /tmp/install-agent.sh && /tmp/install-agent.sh -k \
+"{beszelapi}" -t 84b7-6c9838bc6-4810-523747d38 --auto-update;
+mkdir -p /etc/systemd/system/beszel-agent.service.d;
+cat << EOF > /etc/systemd/system/beszel-agent.service.d/override.conf
+[Service]
+Environment="EXTRA_FILESYSTEMS=/VM,/mnt/Share__SHARE"
+EOF
+systemctl daemon-reload && systemctl restart beszel-agent.service;
+
+
+(crontab -u root -l ; echo "MAILTO={pushoveremail}") | crontab -u root -;
+(crontab -u root -l ; echo "{myemail}") | crontab -u root -;
 (crontab -u root -l ; echo "#0 0 * * * /home/Etienne/HK/Update.sh > /home/Etienne/HK/Update.log 2>&1") | crontab -u root -;
 (crontab -u root -l ; echo "#0 2 * * * /home/Etienne/HK/Backup.sh > /home/Etienne/HK/Backup.log 2>&1") | crontab -u root -;
 (crontab -u root -l ; echo "#0 5 1 * * /home/Etienne/HK/BTRFS.sh > /home/Etienne/HK/BTRFS.log 2>&1") | crontab -u root -;
-(crontab -u root -l ; echo "#*/5 * * * * curl $hcpingurl > /dev/null 2>&1") | crontab -u root -;
+(crontab -u root -l ; echo "#*/5 * * * * curl https://hc-ping.com/{hcpingurl} > /dev/null 2>&1") | crontab -u root -;
 crontab -e;
-
-exit 0;
